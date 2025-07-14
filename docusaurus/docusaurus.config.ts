c398b437ccafd68396805d4b1cae00f976d053a5
antichaosdb <dave@antichaos.co.uk>
2025-07-14 14:43:08 +0100

    chore(docs): update social card image

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: "img/docusaurus-social-card.jpg",
+        image: "images/SciChartLogo.jpg",
         navbar: {
             title: "SciChart JS Docs",
             logo: {
