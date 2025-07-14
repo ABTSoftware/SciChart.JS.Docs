45facbc80979b8b422e5ef2e8b308ab93b167f65
antichaosdb <dave@antichaos.co.uk>
2025-07-14 16:06:35 +0100

    chore(web): fix protocol

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
     themes: ["@docusaurus/theme-mermaid"],
 
     // Set the production url of your site here
-    url: "http://scichart.com",
+    url: "https://scichart.com",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: baseUrl,
