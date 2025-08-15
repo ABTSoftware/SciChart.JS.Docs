f8b093f13ac6dc6a7ec12c0f28d16456d74cee5d
Igor Cuckovic <cuckovic@gmail.com>
2025-08-15 09:38:14 +0200

    docs(update) polar triangle series demo

--- docusaurus/docs/2d-charts/chart-types/polar-triangle-renderable-series/Basic/demo.js
+++ docusaurus/docs/2d-charts/chart-types/polar-triangle-renderable-series/Basic/demo.js
@@ -1,29 +1,73 @@
-import * as SciChart from "scichart";
+import {
+    SciChartPolarSurface,
+    PolarNumericAxis,
+    PolarTriangleRenderableSeries,
+    EPolarAxisMode,
+    XyDataSeries,
+    SciChartJsNavyTheme,
+    ETriangleSeriesDrawMode
+} from "scichart";
+
 export async function PolarTriangleChart(divElementId) {
-    // #region_A_start
     // Demonstrates how to create a basic polar triangle series chart using SciChart.js
-    const { SciChartPolarSurface, PolarNumericAxis, PolarTriangleRenderableSeries, EPolarAxisMode, XyDataSeries, EFillPaletteMode, parseColorToUIntArgb, SciChartJsNavyTheme, ETriangleSeriesDrawMode, NumberRange } = SciChart;
-    // or, for npm, import { SciChartPolarSurface, ... } from "scichart"
+
     const { sciChartSurface, wasmContext } = await SciChartPolarSurface.create(divElementId, {
-        theme: new SciChartJsNavyTheme(),
+        theme: new SciChartJsNavyTheme()
     });
+
+    // #region_A_start
     const radialYAxis = new PolarNumericAxis(wasmContext, {
-        polarAxisMode: EPolarAxisMode.Radial,
-        visibleRange: new NumberRange(0, 5),
+        polarAxisMode: EPolarAxisMode.Radial
     });
+
     sciChartSurface.yAxes.add(radialYAxis);
+
     const angularXAxis = new PolarNumericAxis(wasmContext, {
-        polarAxisMode: EPolarAxisMode.Angular,
+        polarAxisMode: EPolarAxisMode.Angular
     });
+
     sciChartSurface.xAxes.add(angularXAxis);
+
+    const trianglesX = [
+        [566.0, 551.0, 581.0],
+        [529.0, 514.0, 544.0],
+        [483.0, 468.0, 498.0],
+        [445.0, 430.0, 460.0],
+        [409.0, 394.0, 424.0],
+        [399.0, 384.0, 414.0],
+        [401.0, 386.0, 416.0],
+        [412.0, 397.0, 427.0],
+        [437.0, 422.0, 452.0],
+        [473.0, 458.0, 488.0],
+        [527.0, 512.0, 542.0],
+        [574.0, 559.0, 589.0]
+    ].flat(); // xValues and yValues take an array of numbers, and we are using an array of arrays just for readability reasons
+
+    const trianglesY = [
+        [296.32, 270.34, 270.34],
+        [309.32, 283.34, 283.34],
+        [309.32, 283.34, 283.34],
+        [291.32, 265.34, 265.34],
+        [267.32, 241.34, 241.34],
+        [225.32, 199.34, 199.34],
+        [184.32, 158.34, 158.34],
+        [146.32, 120.34, 120.34],
+        [108.32, 82.34, 82.34],
+        [94.32, 68.34, 68.34],
+        [94.32, 68.34, 68.34],
+        [107.32, 81.34, 81.34]
+    ].flat();
+
     const triangle = new PolarTriangleRenderableSeries(wasmContext, {
         dataSeries: new XyDataSeries(wasmContext, {
-            xValues: [0, 2, 3, 4],
-            yValues: [0, 1, 2, 3]
+            xValues: trianglesX,
+            yValues: trianglesY
         }),
-        fill: "green",
-        drawMode: ETriangleSeriesDrawMode.Polygon,
+        fill: "cornflowerblue",
+        opacity: 0.6,
+        drawMode: ETriangleSeriesDrawMode.List
     });
+
     sciChartSurface.renderableSeries.add(triangle);
     // #region_A_end
 }
--- docusaurus/docs/2d-charts/chart-types/polar-triangle-renderable-series/index.md
+++ docusaurus/docs/2d-charts/chart-types/polar-triangle-renderable-series/index.md
@@ -6,6 +6,19 @@ sidebar_position: 105
 
 The [PolarTriangleRenderableSeries:blue_book:](https://www.scichart.com/documentation/js/v4/typedoc/classes/polartrianglerenderableseries.html) creates triangle strips / lists or polyongs in a polar coordinate system, allowing for flexible data visualization in polar charts. This is useful for creating radar charts, polar area charts, or any custom triangular visualizations.
 
+## Creating a Polar Triangle Series
+
+To create a chart using [PolarTriangleRenderableSeries:blue_book:](https://www.scichart.com/documentation/js/v4/typedoc/classes/polartrianglerenderableseries.html) you can use use the following code. 
+
+<CodeSnippetBlock labels={["TS"]}>
+    ```ts {43} showLineNumbers file=./Basic/demo.ts start=#region_A_start end=#region_A_end
+    ```
+</CodeSnippetBlock>
+
+<LiveDocSnippet maxWidth={"100%"} name="./Basic/demo" />
+
+## Polar Map Example
+
 :::tip
 The [JavaScript Polar Map Example](https://stagingdemo2.scichart.com/demo/javascript/polar-map-example) using the `PolarTriangleRenderableSeries` can be found in the [SciChart.Js Examples Suite > Polar Map Example](https://github.com/ABTSoftware/SciChart.JS.Examples/tree/master/Examples/src/components/Examples/Charts2D/PolarCharts/PolarMapExample) on Github, or our live demo at [scichart.com/demo](https://stagingdemo2.scichart.com/demo/react/polar-column-chart).
 :::
