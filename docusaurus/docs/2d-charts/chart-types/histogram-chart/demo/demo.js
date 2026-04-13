fdced5a5b326939242206a5cb16c3b04c8e39361
Igor Cuckovic <cuckovic@gmail.com>
2026-04-14 00:56:34 +0200

    chore: demo.js update

--- docusaurus/docs/2d-charts/chart-types/histogram-chart/demo/demo.js
+++ docusaurus/docs/2d-charts/chart-types/histogram-chart/demo/demo.js
@@ -1,25 +1,8 @@
-import {
-    SciChartSurface,
-    NumericAxis,
-    NumberRange,
-    FastRectangleRenderableSeries,
-    XyxyDataSeries,
-    EColumnMode,
-    EColumnYMode,
-    SciChartJsNavyTheme,
-    GradientParams,
-    Point,
-    ENumericFormat,
-    ZoomPanModifier,
-    ZoomExtentsModifier,
-    MouseWheelZoomModifier
-} from "scichart";
-
+import { SciChartSurface, NumericAxis, NumberRange, FastRectangleRenderableSeries, XyxyDataSeries, EColumnMode, EColumnYMode, SciChartJsNavyTheme, GradientParams, Point, ENumericFormat, ZoomPanModifier, ZoomExtentsModifier, MouseWheelZoomModifier } from "scichart";
 async function histogramChart(divElementId) {
     const { wasmContext, sciChartSurface } = await SciChartSurface.create(divElementId, {
         theme: new SciChartJsNavyTheme()
     });
-
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext, {
         axisTitle: "Age (years)",
         growBy: new NumberRange(0.05, 0.05)
@@ -29,23 +12,21 @@ async function histogramChart(divElementId) {
         labelFormat: ENumericFormat.Engineering,
         growBy: new NumberRange(0, 0.2)
     }));
-
     // region_A_start
     // Histogram bins: each entry defines [x, x1, y] = [binStart, binEnd, count]
     // representing Europe's population by age bracket (simplified)
     const bins = [
-        { x: 0,  x1: 20, y: 150_000_000 },
-        { x: 20, x1: 30, y:  75_000_000 },
+        { x: 0, x1: 20, y: 150_000_000 },
+        { x: 20, x1: 30, y: 75_000_000 },
         { x: 30, x1: 45, y: 112_000_000 },
         { x: 45, x1: 65, y: 138_000_000 },
-        { x: 65, x1: 70, y:  32_000_000 },
-        { x: 70, x1: 80, y:  48_000_000 },
+        { x: 65, x1: 70, y: 32_000_000 },
+        { x: 70, x1: 80, y: 48_000_000 },
     ];
-
     const histogramSeries = new FastRectangleRenderableSeries(wasmContext, {
         dataSeries: new XyxyDataSeries(wasmContext, {
-            xValues:  bins.map(b => b.x),
-            yValues:  bins.map(b => b.y),
+            xValues: bins.map(b => b.x),
+            yValues: bins.map(b => b.y),
             x1Values: bins.map(b => b.x1),
             y1Values: bins.map(() => 0),
         }),
@@ -64,14 +45,7 @@ async function histogramChart(divElementId) {
         }
     });
     // region_A_end
-
     sciChartSurface.renderableSeries.add(histogramSeries);
-
-    sciChartSurface.chartModifiers.add(
-        new ZoomPanModifier(),
-        new ZoomExtentsModifier(),
-        new MouseWheelZoomModifier()
-    );
+    sciChartSurface.chartModifiers.add(new ZoomPanModifier(), new ZoomExtentsModifier(), new MouseWheelZoomModifier());
 }
-
 histogramChart("scichart-root");
